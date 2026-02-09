9e3d490357c6ef5151f3406db8abc6be18cd355c
kyedrafts <kathleendestrizaaa@gmail.com>
2026-02-09 11:21:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -12,6 +12,9 @@ function upDate(previewPic){
     change.style.backgroundImage="url('')";
     document.getElementById('image').innerHTML='Hover over an image below to display here.';
   }
+	window.onload = function refresh(){
+		console.log("The gallery is ready.");
+	};
 
 pics.forEach(img => {
   img.addEventListener('focus', () => upDate(img));
